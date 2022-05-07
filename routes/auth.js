62e5c4bfa943b7ddc125d58d9170052120ec7be8
Andrea Mac <aleatrengrouse@gmail.com>
2022-05-07 20:33:18 +0100

    log in and user update added

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,24 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//login
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("incorrect password");
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,34 @@
 const router = require("express").Router();
-
-router.get("/", (req, res) => {
-  res.send("its user routes");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+// update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been Updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("Update on your account page");
+  }
 });
+//delete user
 
+//get user
+
+//follow a user
+
+//unfollow a user
 module.exports = router;
