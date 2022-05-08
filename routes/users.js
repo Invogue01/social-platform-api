5798692efa475bab6747231fbc8a7b1848d8ae41
Andrea Mac <aleatrengrouse@gmail.com>
2022-05-08 11:56:13 +0100

    find user and follow/unfollow users added

--- routes/users.js
+++ routes/users.js
@@ -39,8 +39,37 @@ router.delete("/:id", async (req, res) => {
   }
 });
 //get user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //follow a user
+
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.UserId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.body.userId } });
+        res.status(200).json("user has been followed");
+      } else {
+        res.status(403).json(" You already following this User");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You cant follow yourself");
+  }
+});
 
 //unfollow a user
 module.exports = router;
