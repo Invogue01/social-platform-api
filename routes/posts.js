3a27521fe62f3351bbc1da9e986637a855365399
Andrea Mac <93999255+Invogue01@users.noreply.github.com>
2022-05-09 14:40:52 +0100

    added likes and dislikes, find post and find entire post timeline

--- routes/posts.js
+++ routes/posts.js
@@ -39,24 +39,59 @@ router.delete("/:id", async(req,res)=>{
     }else{
     res.status(403).json("you can delete only your own posts")
     }
-    } catch {err}
+    } catch (err){
     res.status(500).json(err)
-    
+    }
     })
 
 
-//like post
-
+//like and dislike post
+router.put("/:id/like", async (req,res)=>{
+    try {
+        const post= await Post.findById(req.params.id)
+if(!post.likes.includes(req.body.userId)){
+    await post.udapeOne({$push: {likes: req.body.userId}})
+    res.status(200).jason("Post has been Liked")
+}else {
+    await post.updateOne({$pull: {likes:req.body.userId}})
+    res.status(200).jason("Post has been Disliked")
+}
+    }catch (err){
+        res.status(500).json(err)
+    }
+})
 
 //get a post
 
+router.get("/:id", async (req,res)=>{
+    try{
+const post= await Post.findById(req.params.id)
+res.status(200).json(post)
+    }catch (err){
+res.status(500).jason(err)
+    }
+})
+
 
 //user posts timeline
-
-
-
-router.get("/", (req,res)=>{
-    console.log("post page")
+router.get("/timeline/all", async (req,res)=>{
+    
+    try {
+const currentUser= await User.findById(req.body.userId)
+const userPosts = await Post.find({userId: currentUser._id})
+const friendPosts = await Promise.all(
+    currentUser.followings.map(friendId =>{
+       return Post.find({userId: friendId})
+    })
+    
+)
+res.json(userPosts.concat(...friendPosts))
+    }catch (err){
+        res.status(500).json(err)
+    }
 })
+
+
+
 
 module.exports = router;
